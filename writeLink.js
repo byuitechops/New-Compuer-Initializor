b318f03c6e8696ed146cc3af7a6c4ede82b53d29
Gwinert <gwinert@gmail.com>
2017-04-18 13:22:09 -0600

    Delete writeLink.js

--- writeLink.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-var link = process.argv[2].trim();
-var url = "https://nodejs.org/dist/" + link + "/node-" + link + "-x64.msi"
- 
- fs.writeFile('./node.txt', url ,function(){
-		console.log(url)});
